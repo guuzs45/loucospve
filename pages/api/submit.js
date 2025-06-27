566cfc4c2270888a52d8b64e5fcd62a4e5c9f741
guuzs45 <gufonseca45@gmail.com>
2025-06-27 02:17:57 -0300

    Update submit.js

--- pages/api/submit.js
+++ pages/api/submit.js
@@ -6,7 +6,7 @@ export default async function handler(req, res) {
   const { nome, classe, ip } = req.body;
 
   try {
-    const response = await fetch('https://script.google.com/macros/s/AKfycbxPKdncfnWSseKvIfgB7etmfbkXmEROGksLCGyBjjhxdW1MV7TI44PI89Z6FYCLT-2M/exec', {
+    const response = await fetch('https://script.google.com/macros/s/AKfycbysSNM_WCLXNbOC_v1Oj9ku5jAF0pJWme1K6TtkaeLeMajtxYeHBHuGE_IXSL7LScIX/exec', {
       method: 'POST',
       body: JSON.stringify({ nome, classe, ip }),
       headers: {
